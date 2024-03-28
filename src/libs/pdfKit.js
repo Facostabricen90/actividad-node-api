b2f9adba4dfbb25badf0884b925c4a1d93a00d70
facostabricen90 <facostabricen90@gmail.com>
2024-03-27 19:23:29 -0500

    finishExerciseAPI

--- src/libs/pdfKit.js
+++ src/libs/pdfKit.js
@@ -1,24 +1,20 @@
 import PDFDocument from 'pdfkit';
 import fs from "fs";
-import path from "path"; // Importamos la librería path para trabajar con rutas de archivos
+import path from "path"; 
 
 export function buildPDF(filePath, dataCallBack, endCallBack) {
   const doc = new PDFDocument();
 
-  // Leemos el archivo db.json
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
 
-    const beersData = JSON.parse(data); // Analizamos el contenido JSON
+    const beersData = JSON.parse(data); 
 
-    // Aquí puedes acceder a los datos y pintarlos en el PDF
-    // Por ejemplo, para acceder a la lista de cervezas:
     const beers = beersData.beers;
 
-    // Pintamos los datos de las cervezas en el PDF
     beers.forEach(beer => {
       doc.text(`Name: ${beer.name}`);
       doc.text(`Description: ${beer.descripcion}`);
@@ -29,7 +25,6 @@ export function buildPDF(filePath, dataCallBack, endCallBack) {
       doc.moveDown(); // Moverse a la siguiente línea
     });
 
-    // Cuando terminamos de escribir en el PDF, llamamos a los callbacks
     doc.end();
     doc.on('data', dataCallBack);
     doc.on('end', endCallBack);
